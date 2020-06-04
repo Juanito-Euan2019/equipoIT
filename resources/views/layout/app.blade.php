afe4508e29d7e263f6ae5dec62af1094d5857927
Gerardo36 <47867403+Gerardo36@users.noreply.github.com>
2020-06-03 21:59:13 -0500

    View

---
+++ resources/views/layout/app.blade.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="keywords" content="" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>Tiendita de La esquina</title>
+
+    <!-- favicon icon -->
+
+    <link rel="shortcut icon" href="{{asset('images/favicon.ico')}}"  />
+
+    <!-- inject css start -->
+    <link rel="stylesheet" href="{{asset('style.css')}}">
+    <!--== bulma css -->
+
+    <link href="https://cdn.jsdelivr.net/npm/bulma@0.8.2/css/bulma.css" rel="stylesheet" type="text/css" />
+
+    <!-- inject css end -->
+
+</head>
+
+    <body>
+
+        <div class="page-wrapper">
+
+            @include('partials.header')
+
+            @yield('content')
+
+            @include('partials.footer')
+
+        </div>
+
+        <!-- inject js start -->
+    </body>
+
+</html>
